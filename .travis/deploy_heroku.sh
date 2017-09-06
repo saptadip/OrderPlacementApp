1eb6b1af7c06c955422339bfb5064eab60d3b2ff
saptadip <informsapta@gmail.com>
2017-09-07 00:49:02 +0200

    Updated Deploy to Heroku script.

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -8,5 +8,5 @@ heroku plugins:install heroku-container-registry
 echo "Login to Heroku Docker Registry"
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
 
-echo "Pushing application docker container to Heroku...."
+echo "Pushing application docker container to Heroku"
 heroku container:push web --app $HEROKU_APP_NAME
